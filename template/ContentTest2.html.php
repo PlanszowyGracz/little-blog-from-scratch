a2c45f9cc0ac09f2bbe8307d469e1c8cfa263d77
Przemek Iskra <przemeki7@interia.pl>
2017-11-26 20:48:04 +0100

    checking using bulma

--- template/ContentTest2.html.php
+++ template/ContentTest2.html.php
@@ -1,8 +1,12 @@
-<?php
 
+<section class="section ">
+    <div class="container ">
+        <?php
+        echo "<h1 class=\"title has-text-centered\">content22 </h1>";
+        echo "<h2 class=\"subtitle has-text-centered\"> --- {$data[0]} --- </h2>"
+        ?>
 
-echo "<h1>content22 </h1>";
-echo "<h2> kkk {$data[0]} kkkk</h2>"
-?>
+        <p class="has-text-centered">Hodor! Hodor hodor, hodor - hodor?! Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor hodor? Hodor hodor - hodor hodor hodor? Hodor! Hodor hodor, hodor hodor hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor. </p>
+    </div>
+</section>
 
-<p>Hodor! Hodor hodor, hodor - hodor?! Hodor hodor HODOR! Hodor hodor, hodor. Hodor hodor hodor? Hodor hodor - hodor hodor hodor? Hodor! Hodor hodor, hodor hodor hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor. </p>
--- template/VievTest1.html.php
+++ template/VievTest1.html.php
@@ -6,11 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
     <?php
-    //TODO: To css framework you have planned use bulma, read more.
+
+    //TODO: Decide if you still want using bulma, or not; Eventualny find css framework focused only on layout positioning.
     $script_get();
     $css_get();
     ?>
+    <style>
+        .container{
+            background-color: aquamarine;
+            border-radius: 5px;
+            border: 2px solid black;
+            padding:10px;
+
+        }
+    </style>
 
 </head>
 <body>
