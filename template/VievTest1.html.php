ee1ca3951424b41159d235e7277518b7f8b13134
Przemek Iskra <przemeki7@interia.pl>
2017-12-03 12:25:22 +0100

    mobile menu added, and styled

--- template/VievTest1.html.php
+++ template/VievTest1.html.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 
     <link rel="stylesheet" href="sass/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <style></style>
     <?php
 
--- template/header.html.php
+++ template/header.html.php
@@ -1,8 +1,19 @@
 <?php
 //TODO: make $data  From Model
 //but for now use example dont matter data
-$data=['Co to Litrpg'=>'#','Losowa Recenzja'=>'#','Lista recenzji'=>'#', 'Ranking'=>'#' ];
+$data = ['Co to Litrpg' => '#', 'Losowa Recenzja' => '#', 'Lista recenzji' => '#', 'Ranking' => '#'];
 ?>
+<?php
+function addMobileClass($num)
+{
+    if ($num === 1) {
+        echo "--mobile";
+    }
+}
+
+?>
+
+
 <div class="container-header">
 
     <div class="logo-container">
@@ -13,15 +24,24 @@ $data=['Co to Litrpg'=>'#','Losowa Recenzja'=>'#','Lista recenzji'=>'#', 'Rankin
         </div>
 
     </div>
-    <div class="main-navigation-container">
-        <ul class="main-navigation-list">
-            <?php foreach ($data as $key => $value):?>
-            <li class="main-navigation-list__el">
-                <a class="main-navigation-list__el__link"href="<?php echo $value ?>">
-                    <?php echo $key;?>
-                </a>
-            </li>
-            <?php endforeach;?>
-        </ul>
-    </div>
+    <?php for ($i = 0; $i <= 1; $i++): ?>
+        <?php if ($i === 1) : ?>
+            <span id="main-navigation-mobile-open"><i class="fa fa-bars fa-3x" aria-hidden="true"></i></span>
+        <?php endif; ?>
+        <div class="main-navigation-container<?php addMobileClass($i); ?>">
+            <?php if ($i === 1) : ?>
+                <span id="main-navigation-mobile-close"><i class="fa fa-times fa-3x" aria-hidden="true"></i></span>
+            <?php endif; ?>
+            <ul class="main-navigation-list<?php addMobileClass($i); ?>">
+                <?php foreach ($data as $key => $value): ?>
+                    <li class="main-navigation-list__el<?php addMobileClass($i); ?>">
+                        <a class="main-navigation-list__el__link<?php addMobileClass($i); ?>"
+                           href="<?php echo $value ?>">
+                            <?php echo $key; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endfor; ?>
 </div>
